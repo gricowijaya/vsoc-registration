846debd126714d424fea5980d67e34343e1e3f3e
rico <ge.rico@gmail.com>
2024-07-04 17:09:58 +0800

    fix: python-3.12 installation build from source

--- script/install-wazuh-agent-linux-apt.sh
+++ script/install-wazuh-agent-linux-apt.sh
@@ -19,18 +19,21 @@ echo "Wazuh agent installation completed."
 sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/wazuh.list
 sudo apt-get update
 
-echo "Pacakge wazuh agent update disabled."
+echo "Package wazuh agent update disabled."
 
 # Dependencies for compiling Python
-sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev pkg-config
 
 cd /tmp
 wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tar.xz
+wait
 tar -xf Python-3.12.0.tar.xz
 cd Python-3.12.0
 ./configure --enable-optimizations
 make -j $(nproc)
 sudo make altinstall
+
+sudo ln -sf /usr/local/bin/python3.12 /usr/local/bin/python
 
 wait
 
