276a75555fcb204326324b32fc103e1c80f9ea44
rico <ge.rico@gmail.com>
2024-07-05 04:04:39 +0800

    debug: integer conversion

--- src/utils/socket.py
+++ src/utils/socket.py
@@ -7,8 +7,11 @@ from .output import beautify_json
 def get_agent_ip_hostname(manager_ip: int, manager_port: int):
 	try:
 		name = socket.gethostname()
+		print(name)
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		print(type(manager_ip))
 		s.connect((str(manager_ip), manager_port))
+		print(type(manager_ip))
 		ip = s.getsockname()[0]
 		s.close()
 		data = {"hostname": name, "ip": ip}
