49248bcb33f1d126a7cb60f069e41fdb4f7b77d6
rico <ge.rico@gmail.com>
2024-07-05 04:01:03 +0800

    fix: convert integer to string

--- src/utils/socket.py
+++ src/utils/socket.py
@@ -4,11 +4,11 @@ from .output import beautify_json
 
 @exception_handler
 @beautify_json
-def get_agent_ip_hostname(manager_ip: str, manager_port: int):
+def get_agent_ip_hostname(manager_ip: int, manager_port: int):
 	try:
 		name = socket.gethostname()
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		s.connect((manager_ip, manager_port))
+		s.connect((str(manager_ip), manager_port))
 		ip = s.getsockname()[0]
 		s.close()
 		data = {"hostname": name, "ip": ip}
