7ff87c89ca4e8d15bf9fe3dd86dcdca157458a18
rico <ge.rico@gmail.com>
2024-07-05 02:06:45 +0800

    refactor: readable function list

--- src/integrations/__init__.py
+++ src/integrations/__init__.py
@@ -1,3 +1,13 @@
-from .wazuh_manager import (auth, checkhealth, list_agent, get_agent_key)
+from .wazuh_manager import (auth, 
+							checkhealth, 
+							list_agent, 
+							summary_agents_os, 
+							summary_agents_status, 
+							get_agent_key)
 
-__all__ = ['auth', 'checkhealth', 'list_agent', 'get_agent_key']
+__all__ = ['auth', 
+		   'checkhealth', 
+		   'list_agent', 
+		   'summary_agents_os',
+		   'summary_agents_status',
+		   'get_agent_key']
