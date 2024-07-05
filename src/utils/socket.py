7bbd78cb5c8e36b0a84abc6cce9e0b4eba86316d
rico <ge.rico@gmail.com>
2024-07-05 13:08:05 +0800

    debug: log type of socket

--- src/utils/socket.py
+++ src/utils/socket.py
@@ -9,9 +9,10 @@ def get_agent_ip_hostname(manager_ip: int, manager_port: int):
 		name = socket.gethostname()
 		print(name)
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		print(type(manager_ip))
+		print(f'12, The manager ip is {manager_ip} with type of {type(manager_ip)}')
+		print(f'13, The manager port is {manager_port} with type of {type(manager_ip)}')
 		s.connect((str(manager_ip), manager_port))
-		print(type(manager_ip))
+		print(f'15, The manager ip is {manager_ip} with type of {type(manager_ip)}')
 		ip = s.getsockname()[0]
 		s.close()
 		data = {"hostname": name, "ip": ip}
