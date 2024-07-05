bf759d1c7a03784067ff52c4a6ee04cdf49c56b4
rico <ge.rico@gmail.com>
2024-07-05 13:13:13 +0800

    debug: create management port and address

--- src/utils/socket.py
+++ src/utils/socket.py
@@ -10,10 +10,11 @@ def get_agent_ip_hostname(manager_ip: str, manager_port: str):
 		print(name)
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		print(f'12, {__name__} The manager ip is {manager_ip} with type of {type(manager_ip)}')
-		print(f'13, {__name__}The manager port is {manager_port} with type of {type(manager_ip)}')
-		s.connect(manager_ip, int(manager_port))
+		print(f'13, {__name__}The manager port is {manager_port} with type of {type(manager_port)}')
 		print(f'15, {__name__} The manager ip is {manager_ip} with type of {type(manager_ip)}')
-		print(f'16, {__name__}The manager port is {manager_port} with type of {type(manager_ip)}')
+		manager_port = int(manager_port)
+		print(f'16, {__name__}The manager port is {manager_port} with type of {type(manager_port)}')
+		s.connect(manager_ip, manager_port)
 		ip = s.getsockname()[0]
 		s.close()
 		data = {"hostname": name, "ip": ip}
